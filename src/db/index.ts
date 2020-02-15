a6d751ab36d8132b17de5b9db56da08a74d8968e
Aleksey Sergeev <avatar5555555@gmail.com>
2020-02-15 18:06:26 +0400

    feat: add pg

---
+++ src/db/index.ts
@@ -0,0 +1,9 @@
+import { Client } from "pg";
+
+export const client = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
 import { ApolloServer } from "apollo-server";
 
 import { genSchema } from "./utils/gen-schema";
 
-const schema = genSchema();
+// require dotenv config
+dotenv.config();
 
+const schema = genSchema();
 const server = new ApolloServer(schema as any);
 
 server.listen().then(({ url }) => {
