919e9527b79d597817f0cf4d687e1d8f3dbc13e9
Aleksey Sergeev <avatar5555555@gmail.com>
2020-02-16 10:35:33 +0400

    chore: update server config

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { ApolloServer } from "apollo-server";
 
 import { genSchema } from "./utils/gen-schema";
 
-const schema = genSchema();
+const { resolvers, typeDefs } = genSchema();
 
-const server = new ApolloServer(schema as any);
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  introspection: true,
+  playground: true,
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
