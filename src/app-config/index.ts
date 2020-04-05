eb4b5811b447a7bc42cbfe571160c553bf7e039a
Aleksey Sergeev <avatar5555555@gmail.com>
2020-04-05 20:36:22 +0400

    chore: rollback to dotenv for ECS compatibility

--- src/app-config/index.ts
+++ src/app-config/index.ts
@@ -1,5 +1,21 @@
-import config from "config";
+import dotenv from "dotenv";
 
 import { Config } from "./types";
 
-export const appConfig: Config = config.get("app");
+dotenv.config();
+
+export const appConfig: Config = {
+  tokenExpiresIn: "60min",
+  tokenSecret: process.env["ACCESS_TOKEN_SECRET"] as string,
+  pgUser: process.env["PGUSER"] as string,
+  pgHost: process.env["PGHOST"] as string,
+  pgPassword: process.env["PGPASSWORD"] as string,
+  db: process.env["PGDATABASE"] as string,
+  dbPort: Number(process.env["PGPORT"]),
+  codeExpiresIn: 10,
+  senderEmail: process.env["SENDER"] as string,
+  charset: "UTF-8",
+  useEmailStub: (process.env["USE_EMAIL_STUB"] as string) === "true",
+};
+
+console.log(appConfig);
